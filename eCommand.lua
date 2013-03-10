6c3b68ab24d65b3736f7d4f3637651eaa7aecb2d
Ession <mail@mathiasjost.com>
2013-03-10 21:43:58 +0100

    */rl function - shortcut to /reload
    Signed-off-by: Ession <mail@mathiasjost.com>

--- eCommand.lua
+++ eCommand.lua
@@ -22,3 +22,16 @@ SLASH_ECOMMAND_RDY1 = "/rdy"
 SlashCmdList["ECOMMAND_RDY"] = function(msg, editBox)
 	DoReadyCheck()
 end
+
+
+-- -----------------------------------------------------------------------------
+-- Reload
+-- 
+-- Performs a UI Reload
+-- 
+-- /rl
+-- -----------------------------------------------------------------------------
+SLASH_ECOMMAND_RL1 = "/rl"
+SlashCmdList["ECOMMAND_RL"] = function(msg, editBox)
+	ReloadUI()
+end
