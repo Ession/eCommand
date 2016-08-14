4ba472d62dfeb194d3636eafe55deabc1fecbb4e
Ession <mail@mathiasjost.com>
2016-08-14 03:44:51 +0200

    * Bumped Version for Legion
    Signed-off-by: Ession <mail@mathiasjost.com>

--- eCommand.lua
+++ eCommand.lua
@@ -4,7 +4,7 @@
 --
 --    Description:  Provides Several Slashcommands to simplyfi some tasks.
 --
---        Version:  6.2.1
+--        Version:  7.0.1
 --
 --         Author:  Mathias Jost (mail@mathiasjost.com)
 --
